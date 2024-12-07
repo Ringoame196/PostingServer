839799fd7095ab32df9186ed1eaf7b59f072dac9
りんご飴196 <kazumac41@gmail.com>
2024-12-07 17:52:18 +0900

    Initial commit

---
+++ src/main/java/com/github/ringoame196/Main.kt
@@ -0,0 +1,40 @@
+package org.example
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.io.File
+
+object Main {
+	@JvmStatic
+	fun main(args: Array<String>) {
+		val defaultPort = 8080
+		val port = args[0].toIntOrNull() ?: defaultPort
+
+		// サーバーを起動
+		embeddedServer(Netty, port = port) {
+			routing {
+				// ルートにアクセスされた場合、ファイルを公開
+				static("/") {
+					// JARファイル直下のパスを指定
+					val jarDir = File(System.getProperty("user.dir"))
+					val publicDir = File(jarDir, "public")
+
+					if (!publicDir.exists()) publicDir.mkdirs()
+
+					// 静的ファイルを提供
+					staticRootFolder = publicDir
+					files(".")
+				}
+
+				// 任意のエンドポイントを作成可能
+				get("/") {
+					call.respondText("ファイルサーバーが稼働中です")
+				}
+			}
+		}.start(wait = true)
+	}
+}
