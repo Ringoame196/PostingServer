46dd700141f35882501ea13d98099703d21b9d4a
りんご飴196 <kazumac41@gmail.com>
2024-12-07 17:56:51 +0900

    ⬆️ : Kotlinに

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,37 @@
 plugins {
-    id("java")
+	id("java")
+	kotlin("jvm")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
+	implementation(kotlin("stdlib-jdk8"))
+	implementation("io.ktor:ktor-server-core:2.3.3")
+	implementation("io.ktor:ktor-server-netty:2.3.3")
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
+}
+
+tasks.jar {
+	manifest {
+		attributes["Main-Class"] = "com.github.ringoame196.MainKt"
+	}
+	configurations["compileClasspath"].forEach { file: File ->
+		from(zipTree(file.absoluteFile))
+	}
+	duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+kotlin {
+	jvmToolchain(22)
 }
